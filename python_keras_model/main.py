4a53187e5517ff0ea88ca0676c4064b882973a58
Ian M <44310933+mnai01@users.noreply.github.com>
2023-05-25 15:57:21 -0400

    add key_point_indices to main

--- python_keras_model/main.py
+++ python_keras_model/main.py
@@ -21,20 +21,25 @@ exercise_key_points = {
 
 exercise = "squat"
 key_points = exercise_key_points[exercise]["keypoints"]
-key_point_indices = [kp.value for kp in key_points]
+# _all_landmarks_as_dataframe func in pose_estimation_preprocessing.py will add the Bodyparts 
+# to the csv training data based on its order.
+# Body part starts at 0 and goes to 16 filtering out the items we dont need
+# In this case, our key_point_indices need to be in sorted order so it will match the csv training data
+# sorting this will results into a key_point_indices index / column match (after unneccesary columns like file_name are removed)
+# so if left_shoulder is the first body part in the columns then left_shoulder key_point_indices index is 0
+key_point_indices = sorted([kp.value for kp in key_points])
 dataset_in = exercise_key_points[exercise]["image_set_dir"]
 # Create a folder for each exercise's model
 model_output_dir = f'./models/{exercise}_model'
 os.makedirs(model_output_dir, exist_ok=True)
 
 root = split_init(dataset_in)
-print(root)
 csvs_out_test_path, csvs_out_train_path = generate_set(root, train_folder='train', test_folder='test', key_point_indices=key_point_indices)
 
 # Check if tfjs model already exists
 tfjs_model_path = os.path.join(model_output_dir, 'model.json')
 if not os.path.isfile(tfjs_model_path):    
-    model, history = create_and_train_model(csvs_out_test_path=csvs_out_test_path, csvs_out_train_path=csvs_out_train_path)
+    model, history = create_and_train_model(csvs_out_test_path=csvs_out_test_path, csvs_out_train_path=csvs_out_train_path, test_size=0.15, key_point_indices=key_point_indices)
     convert_model(model, model_output_dir=model_output_dir)
 else:
     print(f"Model '{tfjs_model_path}' already exists, skipping training and conversion.")
