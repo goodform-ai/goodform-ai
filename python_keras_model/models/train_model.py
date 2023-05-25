4a052924b1414434bb628892307812e6ccbd0768
Ian M <44310933+mnai01@users.noreply.github.com>
2023-05-25 15:56:27 -0400

    update model for key_point_indices

--- python_keras_model/models/train_model.py
+++ python_keras_model/models/train_model.py
@@ -3,59 +3,75 @@ from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from models.pose_normalization import landmarks_to_embedding, load_pose_landmarks
 import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+import datetime
 
-def create_and_train_model(csvs_out_train_path, csvs_out_test_path, test_size=0.15, num_key_points=17):
+def create_and_train_model(csvs_out_train_path, csvs_out_test_path, key_point_indices, test_size=0.15):
     """Create and train the model."""
 
-    # Load the train data
+    # Load training data
     X, y, class_names, _ = load_pose_landmarks(csvs_out_train_path)
 
-    # Split training data (X, y) into (X_train, y_train) and (X_val, y_val)
+    # Split training data into training and validation sets
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size)
 
-    # Load the test data
+    # Load test data
     X_test, y_test, _, df_test = load_pose_landmarks(csvs_out_test_path)
 
-    input_size = num_key_points * 3
-    inputs = tf.keras.Input(shape=(input_size))
-    embedding = landmarks_to_embedding(inputs, num_key_points)
+    # Define the input size as the number of key points multiplied by 3
+    # (x, y coordinates and a confidence score for each key point)
+    input_size = len(key_point_indices) * 3
 
-    # Modify the number of nodes in the Dense layers based on the input size
+    # Define the input layer
+    inputs = tf.keras.Input(shape=(input_size))
+
+    # Use the landmarks_to_embedding function to process input data
+    embedding = landmarks_to_embedding(inputs, key_point_indices=key_point_indices)
+
+    # Define the neural network layers
+    # The number of nodes in the Dense layers are based on the input size
     layer = keras.layers.Dense(128, activation=tf.nn.relu6)(embedding)
     layer = keras.layers.Dropout(0.5)(layer)
     layer = keras.layers.Dense(64, activation=tf.nn.relu6)(layer)
     layer = keras.layers.Dropout(0.5)(layer)
-    outputs = keras.layers.Dense(len(class_names), activation="sigmoid")(layer)
 
+    # The output layer has as many nodes as there are classes
+    outputs = keras.layers.Dense(len(class_names), activation="softmax")(layer)
+
+    # Define the model
     model = keras.Model(inputs, outputs)
     model.summary()
 
-    # Compile and train model with training data
+    # Compile the model with the optimizer, loss function and metric
     model.compile(
         optimizer='adam',
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
 
-    # Add a checkpoint callback to store the checkpoint that has the highest
-    # validation accuracy.
+    # Create a checkpoint callback to save the best model (highest validation accuracy)
     checkpoint_path = "weights.best.hdf5"
     checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                  monitor='val_accuracy',
                                                  verbose=1,
                                                  save_best_only=True,
                                                  mode='max')
-    earlystopping = keras.callbacks.EarlyStopping(monitor='val_accuracy',
-                                                  patience=20)
 
-    # Start training
+    # Early stopping callback to stop training when validation accuracy does not improve for a certain number of epochs
+    earlystopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=20)
+
+    # TensorBoard callback for visualizing training progress
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    # Train the model
     history = model.fit(X_train, y_train,
                         epochs=200,
                         batch_size=16,
                         validation_data=(X_val, y_val),
-                        callbacks=[checkpoint, earlystopping])
+                        callbacks=[checkpoint, earlystopping, tensorboard_callback])
 
-    # Save the best model
+    # Load the best model (highest validation accuracy) from the checkpoint
     best_model = tf.keras.models.load_model(checkpoint_path)
 
     return best_model, history
